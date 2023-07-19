6e808ebd2cbc7e444d3507940ba2f4ce1cb8bb20
Vaibhav soni <vaibhavjain3999@gmail.com>
2023-07-18 17:20:57 -0700

    fixed QR Code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Homepage from "./pages/Homepage";
 import Manage from "./pages/Manage";
 import Notfound from "./pages/Notfound";
 import Login from "./pages/Login";
+import Generator from "./pages/Generator";
 
 
 
@@ -16,7 +17,7 @@ export default function App() {
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
-  
+
 
   useEffect(() => {
     const fetchSession = async () => {
@@ -51,90 +52,101 @@ export default function App() {
   return (
     <Router>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Linkmate
-          </span>
-        </Link>
-        <button
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          onClick={handleToggle}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="/" className="flex items-center">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Linkmate
+            </span>
+          </Link>
+          <button
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            onClick={handleToggle}
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div
-          className={`w-full justify-end md:w-auto md:flex-grow md:flex md:items-center transition duration-300 ease-in-out ${
-            isOpen ? "block" : "hidden"
-          }`}
-        >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className="block py-2 pl-3 pr-4  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            {user ? (
-              <>
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className={`w-full justify-end md:w-auto md:flex-grow md:flex md:items-center transition duration-300 ease-in-out ${isOpen ? "block" : "hidden"
+              }`}
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 pl-3 pr-4  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              {user ? (
+                <>
+                  <li>
+                    <Link
+                      to="/manage"
+                      className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                      aria-current="page"
+                    >
+                      Manage
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      to="/generator"
+                      className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                      aria-current="page"
+                    >
+                      Generator
+                    </Link>
+                  </li>
+
+                  <li>
+                    <button
+                      onClick={handleLogout}
+                      className="block py-2 pl-3 pr-4  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                      aria-current="page"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </>
+              ) : (
                 <li>
                   <Link
-                    to="/manage"
+                    to="/login"
                     className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                     aria-current="page"
                   >
-                    Manage
+                    Login
                   </Link>
                 </li>
-                <li>
-                  <button
-                    onClick={handleLogout}
-                    className="block py-2 pl-3 pr-4  rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    Logout
-                  </button>
-                </li>
-              </>
-            ) : (
-              <li>
-                <Link
-                  to="/login"
-                  className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Login
-                </Link>
-              </li>
-            )}
-          </ul>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
 
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
-        {user && <Route path="/manage" element={<Manage />} />}
+        {user && <> <Route path="/manage" element={<Manage />} />
+          <Route path="/generator" element={<Generator />} /></>}
         <Route path="*" element={<Notfound />} />
       </Routes>
     </Router>
